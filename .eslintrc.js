a55f3748963568929abd145bcd87ecf2d4d862e5
Sungjoon Park <sungjoon512@gmail.com>
2020-09-07 22:36:08 +0900

    add and edit prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  extends: 'standard-with-typescript',
+  extends: [
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   parserOptions: {
     project: './tsconfig.json',
   },
